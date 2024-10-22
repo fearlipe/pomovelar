ce884e73443fe7dcaefb645be46f70949ec83be6
Felipe Avelar <felipe@avelar.me>
2024-10-22 05:31:36 -0300

    Initial Commit

---
+++ pomovelar/pomovelarApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomovelarApp.swift
+//  pomovelar
+//
+//  Created by Felipe Avelar on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct pomovelarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
